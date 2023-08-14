2dba62bbd6ea08883e77bc66d4ea9490c907a755
Eitan <eitancohen77@gmail.com>
2023-08-14 08:24:46 -0400

    Fixed pages

--- home.php
+++ home.php
@@ -11,95 +11,120 @@
 </head>
 
 <body>
-    <div class="tweetInformation">
-        <form id="tweetForm" name="tweetForm" action="home.php" method="POST">
-            <textarea type="text" name="tweet" id="tweetContent" placeholder="What is happening?!" required></textarea>
-            <button for="tweetForm" id='postButton' type="submit">Post</button>
-        </form>
-    </div>
 
-    <div id="tweetsContainer"></div>
+    <div id="homeInfo">
 
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "12321";
-    $db = "twitterClone";
+        <div id="navbar">
+            <div id="homeTag">Home</div>
+        </div>
 
-    $conn = new mysqli($servername, $username, $password, $db);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+        <div id="mainInfo">
 
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        if (isset($_POST["tweet"])) {
-            $tweet = $_POST["tweet"];
-            $sql = "INSERT INTO tweets (tweet) VALUES ('$tweet')";
+            <div class="tweetInformation">
+                <div id="profilePic">
 
-            if ($conn->query($sql) === TRUE) {
-                echo "Tweet inserted successfully";
-
-                // we redirect the user back into the page that way everytime he refreshes, it wont
-                // activate the previous POST method and cause multiple tweets based off of refreshes
-                header("Location: home.php");
-                exit();
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-        }
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["delete_tweet"])) {
-        $tweet_id = $_POST["tweet_id"];
-        $sql = "DELETE FROM tweets WHERE id = '$tweet_id'";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Tweet deleted successfully";
-            header("Location: home.php");
-            exit();
-        } else {
-            echo "Error deleting tweet: " . $conn->error;
-        }
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["edit_tweet"])) {
-        $tweet_id = $_POST["tweet_id"];
-        $sql = "DELETE FROM tweets WHERE id = '$tweet_id'";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Tweet deleted successfully";
-            header("Location: home.php");
-            exit();
-        } else {
-            echo "Error deleting tweet: " . $conn->error;
-        }
-    }
-
-
-    $get_tweets_query = "SELECT * FROM tweets";
-    $results = $conn->query($get_tweets_query);
-
-    echo "<ul>";
-    while ($row = $results->fetch_assoc()) {
-        $tweet_id = $row["id"];
-        echo "
-            <div class='postedTweets'>
-                <div class='userInfo'>
-                    <div class='user_name'><b>Eitan Cohen</b></div>
-                    <div style='color:rgba(150, 150, 150);' class='username'>@EitantakesCISC3140</div>
                 </div>
-                <p>" . $row["tweet"] . "
-                <form method='post' style='display: inline-block;'>
-                    <input type='hidden' name='tweet_id' value='$tweet_id'>
-                    <button style='width:20px; height: 20px; background-color: red;' type='submit' name='delete_tweet'></button>
-                    <button type='submit' name='edit_tweet'> Edit Tweet</button>
+                <form id="tweetForm" name="tweetForm" action="home.php" method="POST">
+                    <textarea type="text" name="tweet" id="tweetContent" placeholder="What is happening?!" required></textarea>
+                    <button for="tweetForm" id='postButton' type="submit">Post</button>
                 </form>
             </div>
-            ";
-    }
-    echo "<ul>";
 
-    ?>
+            <div id="tweetsContainer"></div>
+
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "12321";
+            $db = "twitterClone";
+
+            $conn = new mysqli($servername, $username, $password, $db);
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                if (isset($_POST["tweet"])) {
+                    $tweet = $_POST["tweet"];
+                    $sql = "INSERT INTO tweets (tweet) VALUES ('$tweet')";
+
+                    if ($conn->query($sql) === TRUE) {
+                        echo "Tweet inserted successfully";
+
+                        // we redirect the user back into the page that way everytime he refreshes, it wont
+                        // activate the previous POST method and cause multiple tweets based off of refreshes
+                        header("Location: home.php");
+                        exit();
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+                }
+            }
+
+            if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["delete_tweet"])) {
+                $tweet_id = $_POST["tweet_id"];
+                $sql = "DELETE FROM tweets WHERE id = '$tweet_id'";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "Tweet deleted successfully";
+                    header("Location: home.php");
+                    exit();
+                } else {
+                    echo "Error deleting tweet: " . $conn->error;
+                }
+            }
+
+            if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["edit_tweet"])) {
+                $tweet_id = $_POST["tweet_id"];
+                $sql = "DELETE FROM tweets WHERE id = '$tweet_id'";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "Tweet deleted successfully";
+                    header("Location: home.php");
+                    exit();
+                } else {
+                    echo "Error deleting tweet: " . $conn->error;
+                }
+            }
+
+
+            $get_tweets_query = "SELECT * FROM tweets";
+            $results = $conn->query($get_tweets_query);
+
+            echo "<ul>";
+            while ($row = $results->fetch_assoc()) {
+                $tweet_id = $row["id"];
+                echo "
+                    <div class='postedTweets'>
+                        <div class='userInfo'>
+                            <div class='user_name'><b>Eitan Cohen</b></div>
+                            <div style='color:rgba(150, 150, 150);' class='username'>@EitantakesCISC3140</div>
+                        </div>
+                        <p>" . $row["tweet"] . "</p>
+                        <table>
+                            <tr>
+                                <td>
+                                    <form method='post' style='display: inline-block;'>
+                                        <input type='hidden' name='tweet_id' value='$tweet_id'>
+                                        <button class='deleteButton' type='submit' name='delete_tweet'>Delete</button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <form method='post' style='display: inline-block;'>
+                                        <input type='hidden' name='tweet_id' value='$tweet_id'>
+                                        <button class='editButton' type='submit' name='edit_tweet'> Edit Tweet</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                ";
+            }
+            echo "<ul>";
+            ?>
+        </div>
+    </div>
+
 
 
     <script src="app.js"></script>
--- signup.php
+++ signup.php
@@ -14,14 +14,58 @@
         <div>
             <a href="/home.php">MAKE THIS AN IMAGE LATER</a>
             <h1>Join Twitter Today</h1>
-            <button id="nextButton"><b>Create account</b></button>
-            </form>
+
+            <!-- Step 1: Create account button -->
+            <button class='styledButton' id="createAccountButton"><b>Create account</b></button>
+
+            <div id="additionalFields" style="display: none;">
+                <input type="text" id="nameField" placeholder="Enter your name">
+                <input type="text" id="dobField" placeholder="Enter your date of birth">
+                <input type="text" id="usernameField" placeholder="Enter your username" style="display: none;">
+                <input type="password" id="passwordField" placeholder="Enter your password" style="display: none;">
+
+                <button id="nextButton"><b>Next</b></button>
+
+                <button id="submitButton" style="display: none;"><b>Sign Up</b></button>
+            </div>
+
             <div id="signUpSection">
                 <p style="margin: 0;">Have an account already?</p>
-                <a style="margin-left: 2px;"href="/login.php">Log in</a>
+                <a style="margin-left: 2px;" href="/login.php">Log in</a>
             </div>
         </div>
     </div>
 
+    <script>
+        const createAccountButton = document.getElementById('createAccountButton');
+        const nextButton = document.getElementById('nextButton');
+        const submitButton = document.getElementById('submitButton');
+        const additionalFields = document.getElementById('additionalFields');
+        const usernameField = document.getElementById('usernameField');
+        const passwordField = document.getElementById('passwordField');
+        const nameField = document.getElementById('nameField');
+        const dobField = document.getElementById('dobField');
+
+        createAccountButton.addEventListener('click', () => {
+            // Show additional fields and the next button
+            additionalFields.style.display = 'block';
+            nextButton.style.display = 'block';
+            // Hide the create account button
+            createAccountButton.style.display = 'none';
+        });
+
+        nextButton.addEventListener('click', () => {
+            // Show username and password fields
+            usernameField.style.display = 'block';
+            passwordField.style.display = 'block';
+            // Show the submit button
+            submitButton.style.display = 'block';
+            // Hide the next button
+            nextButton.style.display = 'none';
+            nameField.style.display = 'none'
+            dobField.style.display = 'none'
+        });
+    </script>
 </body>
+
 </html>
